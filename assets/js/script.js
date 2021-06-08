861e8537399c2636fadc9261bc7d478073ce0eb1
OBASTIAS <dj.osckaar@gmail.com>
2021-06-07 22:25:51 -0400

    second Commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+    $('body a').click(function(e) {
+        e.preventDefault(); //evitar el eventos del enlace normal
+        var strAncla = $(this).attr('href'); //id del ancla
+        $('body,html').stop(true, true).animate({
+            scrollTop: $(strAncla).offset().top
+        }, 800);
+    });
+});
+
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
